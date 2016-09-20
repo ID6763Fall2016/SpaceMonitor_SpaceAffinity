3437e18475f8de091838dc5fda8c74348e0c511a
amy <yiming3478@gmail.com>
2016-09-20 15:49:09 -0400

    Added testing file

--- sensors/touchSensor.js
+++ sensors/touchSensor.js
@@ -1,5 +1,5 @@
 const MPR121 = require('adafruit-mpr121'),
-      mpr121  = new MPR121(0x5A, 1);
+      mpr121  = new MPR121(0x29, 1);
 
 // listen for touch events
 mpr121.on('touch', (pin) => console.log(`pin ${pin} touched`));
