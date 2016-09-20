322719513c2f977197a7cfcb33db3d9f6f9cef93
amy <yiming3478@gmail.com>
2016-09-20 16:17:31 -0400

    Added testing file

--- sensors/touchSensor.js
+++ sensors/touchSensor.js
@@ -1,18 +1,18 @@
-const MPR121 = require('adafruit-mpr121'),
-      mpr121  = new MPR121(0x29, 1);
+// const MPR121 = require('adafruit-mpr121'),
+//       mpr121  = new MPR121(0x29, 1);
 
-// listen for touch events
-mpr121.on('touch', (pin) => console.log(`pin ${pin} touched`));
+// // listen for touch events
+// mpr121.on('touch', (pin) => console.log(`pin ${pin} touched`));
 
-// listen for release events
-mpr121.on('release', (pin) => console.log(`pin ${pin} released`));
+// // listen for release events
+// mpr121.on('release', (pin) => console.log(`pin ${pin} released`));
 
-// listen for changes to the state of a specific pin
-mpr121.on(3, (state) => console.log(`pin 3 is ${state ? 'touched' : 'released'}`));
+// // listen for changes to the state of a specific pin
+// mpr121.on(3, (state) => console.log(`pin 3 is ${state ? 'touched' : 'released'}`));
 
-// check the current state of a specific pin synchronously
-const state = mpr121.isTouched(2);
-console.log(`pin 2 is ${state ? 'touched' : 'released'}`);
+// // check the current state of a specific pin synchronously
+// const state = mpr121.isTouched(2);
+// console.log(`pin 2 is ${state ? 'touched' : 'released'}`);
 
 // var raspi = require('raspi');
 // var I2C = require('raspi-i2c').I2C;
@@ -25,3 +25,39 @@ console.log(`pin 2 is ${state ? 'touched' : 'released'}`);
 //   	console.log(i2c.readByteSync(0x29)); 
 //   }, 2000);
 // });
+
+
+var i2c = require('i2c-bus'),
+  i2c1 = i2c.openSync(1);
+ 
+var DS1621_ADDR = 0x29,
+	CMD_ACCESS_CONFIG = 0xac,
+	CMD_READ_TEMP = 0xaa,
+	CMD_START_CONVERT = 0xee;
+
+
+(function () {
+  var rawTemp;
+ 
+  // Enter one shot mode (this is a non volatile setting) 
+  i2c1.writeByteSync(DS1621_ADDR, CMD_ACCESS_CONFIG, 0x01);
+ 
+  // Wait while non volatile memory busy 
+  while (i2c1.readByteSync(DS1621_ADDR, CMD_ACCESS_CONFIG) & 0x10) {
+  }
+ 
+  // Start temperature conversion 
+  i2c1.sendByteSync(DS1621_ADDR, CMD_START_CONVERT);
+ 
+  // Wait for temperature conversion to complete 
+  while ((i2c1.readByteSync(DS1621_ADDR, CMD_ACCESS_CONFIG) & 0x80) === 0) {
+  }
+ 
+  // Display temperature 
+  rawTemp = i2c1.readWordSync(DS1621_ADDR, CMD_READ_TEMP);
+  console.log('temp: ' + rawTemp);
+ 
+  i2c1.closeSync();
+}());
+
+
