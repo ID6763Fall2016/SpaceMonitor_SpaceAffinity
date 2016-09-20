d99e12feabe0cca123a65bc060b777355c3b447c
amy <yiming3478@gmail.com>
2016-09-20 15:52:47 -0400

    Added testing file

--- sensors/touchSensor.js
+++ sensors/touchSensor.js
@@ -19,5 +19,9 @@ var I2C = require('raspi-i2c').I2C;
  
 raspi.init(function() {
   var i2c = new I2C();
-  console.log(i2c.readByteSync(0x29)); // Read one byte from the device at address 18
+  // Read one byte from the device at address 18
+
+  setInterval(function() {
+  	console.log(i2c.readByteSync(0x29)); 
+  }, 2000);
 });
